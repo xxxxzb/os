5b2b4f439cc1bafd2f581dafefe1069c816739ea
xxxxzb <benzjf@live.com>
2020-03-10 15:52:27 +0800

    手动触发断点中断

--- src/boot/entry64.asm
+++ src/boot/entry64.asm
@@ -1,16 +1,17 @@
 /*设置内核运行环境*/
 
     .section .text.entry
-    .globl _start
+    .global _start
 _start:
     la sp, bootstacktop
     call rust_main
-
 
     .section .bss.stack
     .align 12
     .global bootstack
 bootstack:
+
+
     .space 4096 * 4
     .global bootstacktop
 bootstacktop:
